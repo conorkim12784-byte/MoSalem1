2d15f74feb01282c7c4bcf8dd5e5cdf4fd5b4cef
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-04 01:55:50 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+import logging
+
+from pyrogram import Client, idle
+from pyrogram.errors.exceptions.bad_request_400 import BadRequest
+
+from config import TOKEN, disabled_plugins, log_chat, API_ID, API_HASH
+from utils import get_restarted, del_restarted
+
+with open("version.txt") as f:
+    version = f.read().strip()
+
+
+client = Client("Cv_BeTa", API_ID, API_HASH,
+                bot_token=TOKEN,
+                workers=24,
+                parse_mode="html",
+                plugins=dict(root="plugins", exclude=disabled_plugins))
+
+with client:
+    if __name__ == "__main__":
+        # client.me = client.get_me()
+        wr = get_restarted()
+        xxx = "Cv_BeTa"
+        del_restarted()
+        try:
+            print("Bot started\n"
+                  f"Version: {version}")
+            if wr:
+                client.edit_message_text(wr[0], wr[1], "Restarted successfully.")
+        except BadRequest:
+            logging.warning("Unable to send message to log_chat.")
+        idle()
